192522a4a341ef262274de0813f8e09f6457854f
刘白菜 <945725069@qq.com>
2016-04-22 18:54:26 +0800

    update

--- app.rb
+++ app.rb
@@ -15,7 +15,7 @@ class Post < ActiveRecord::Base
   validates :body, presence: true
 end
 
-class Config < ActiveRecord::Base
+class AppConfig < ActiveRecord::Base
   self.table_name = "configs"
 end
 
---
+++ db/migrate/20160422163133_create_app_configs.rb
@@ -0,0 +1,13 @@
+class CreateAppConfigs < ActiveRecord::Migration
+ def self.up
+   create_table :app_configs do |t|
+     t.string :ckey
+     t.string :cvalue
+     t.timestamps
+   end
+ end
+
+ def self.down
+   drop_table :app_configs
+ end
+end
