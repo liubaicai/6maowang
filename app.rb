7182341fc41598eeffec05bb8fa51bca58f7e4c0
liubaicai <945725069@qq.com>
2016-04-24 02:19:47 +0800

    update

--- app.rb
+++ app.rb
@@ -182,7 +182,7 @@ post "/image/upload" do
   unless params[:file] && (tempfile = params[:file][:tempfile])
     "NULL"
   end
-  params
+  tempfile.methods
   # begin 
   #   #要上传的空间
   #   bucket = "www-6mao-wang"
