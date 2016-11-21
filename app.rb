606c0c62058aaa2f0ba24ee674866ddb689df7d4
刘白菜 <liushuai.baicai@hotmail.com>
2016-11-21 16:44:23 +0800

    add files

--- app.rb
+++ app.rb
@@ -8,7 +8,28 @@ class Photo < ActiveRecord::Base
   belongs_to :gallery
 end
 
+use Rack::Session::Pool, :expire_after => 2592000
+$page_size = 12
+
+after do
+  ActiveRecord::Base.connection.close
+end
+
 get '/' do
-  @test = 'galleries :'+Gallery.count.to_s+',photos :'+Photo.count.to_s
+  @galleries = Gallery.order("created_at DESC").limit($page_size)
+  t_count = Gallery.count
+  @pre_no = ""
+  @pre_class = "class=\"disabled\""
+  if t_count > $page_size
+    @nxt_no = "href=\"/page/2\""
+    @nxt_class = ""
+  else
+    @nxt_no = ""
+    @nxt_class = "class=\"disabled\""
+  end
   erb :'index'
+end
+
+get '/galleries/:id' do
+  "Hello World"
 end
