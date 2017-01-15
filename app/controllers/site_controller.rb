db0e6829d448ce7a45492c75cbbcfacda8562a72
刘白菜 <945725069@qq.com>
2017-01-15 21:04:31 +0800

    update

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -41,7 +41,7 @@ class SiteController < ApplicationController
         end
 
         bucket = $OSS_Client.get_bucket('6mao')
-        filename = "photos/#{Time.now.to_i}.jpg"
+        filename = "photos/#{params[:name]}-#{Time.now.to_i}.jpg"
         bucket.put_object(filename, :file => tempfile)
         file_url = URI.decode(bucket.object_url(filename, false))
         #file_url = 'https://ss0.bdstatic.com/5aV1bjqh_Q23odCf/static/superman/img/logo/bd_logo1_31bdc765.png'
