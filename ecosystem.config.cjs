757e51fabcb6d4071031df7320021b234a720bbc
刘白菜 <liushuai.baicai@hotmail.com>
2025-12-23 19:04:20 +0800

    update env

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -6,11 +6,6 @@ module.exports = {
       exec_mode: 'fork',
       instances: 1,
       watch: false,
-      env: {
-        NODE_ENV: 'production',
-        PORT: process.env.PORT || 3000,
-        SESSION_SECRET: process.env.SESSION_SECRET || 'change_this_session_secret',
-      },
       out_file: 'logs/app.out.log',
       error_file: 'logs/app.err.log',
       log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
