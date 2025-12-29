3a4ca95ad4c4af785c36813d5168b2903799d00b
刘白菜 <liushuai.baicai@hotmail.com>
2025-12-29 18:38:02 +0800

    fix: 移除 S3 标识的显示逻辑

--- app/components/PhotoCard.vue
+++ app/components/PhotoCard.vue
@@ -13,14 +13,6 @@
         class="w-full h-full object-cover transition-transform duration-300 group-hover:scale-105"
         loading="lazy"
       />
-      <!-- S3 标识 -->
-      <div 
-        v-if="photo.hasS3" 
-        class="absolute top-2 right-2 w-5 h-5 bg-purple-500/80 rounded-full flex items-center justify-center"
-        title="已同步到 S3"
-      >
-        <UIcon name="i-heroicons-cloud" class="w-3 h-3 text-white" />
-      </div>
     </div>
     
     <!-- 信息 -->
